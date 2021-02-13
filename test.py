0741b41499ec00e43dbaeafe2ddae2c53083598c
homolyapanni <78760606+homolyapanni@users.noreply.github.com>
2021-02-13 10:32:12 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+
 def test(train_set,val_set):
+  train_data = read_data(train_set)
+  val_data = read_data(dev_set)
+
+  featurizer = Featurizer()
+  train_features, train_labels = featurizer.featurize(
+        train_data, allow_new_features=True)
+   
+  val_features, val_labels = featurizer.featurize(
+     val_data, allow_new_features=False)
+
+  model = LogisticRegression()
+  model.fit(train_features, train_labels)
+
+  predictions = model.predict(val_features)
+  print(classification_report(val_labels, predictions))
