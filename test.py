01df148a3db827574802cf20653c71b0c02ff987
homolyapanni <78760606+homolyapanni@users.noreply.github.com>
2021-02-15 10:46:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 from sklearn.metrics import classification_report
 from sklearn.linear_model import LogisticRegression
 
-def test(train_set,val_set):
-  train_data = read_data(train_set)
-  val_data = read_data(dev_set)
+def test(path):
+    dataset = read_data(path)
+    
+    clead_data = pre_processing(dataset)
+    
+    train_data, val_data, test_data = split_data(clean_data)
 
-  featurizer = Featurizer()
-  train_features, train_labels = featurizer.featurize(
-        train_data, allow_new_features=True)
+    featurizer = Featurizer()
+    train_features, train_labels = featurizer.featurize(train_data, allow_new_features=True)
    
-  val_features, val_labels = featurizer.featurize(
-     val_data, allow_new_features=False)
+    val_features, val_labels = featurizer.featurize(val_data, allow_new_features=False)
 
-  model = LogisticRegression()
-  model.fit(train_features, train_labels)
+    model = LogisticRegression()
+    model.fit(train_features, train_labels)
 
-  predictions = model.predict(val_features)
-  print(classification_report(val_labels, predictions))
+    predictions = model.predict(val_features)
+    print(classification_report(val_labels, predictions))
